86d796c04f7115b8824d38044cf1d27dcdbc258e
tareksakakini <tarek.sakakini@gmail.com>
2015-03-14 02:36:49 +0100

    Synced with VM

--- Wellner_Parser/README.markdown
+++
@@ -1,4 +0,0 @@
-# Wellner PDTB Head Extraction #
-## Python implementation of Ben Wellner's Penn Discourse Treebank head extraction algorithm ##
-
-Please see the [project homepage](http://ymasory.github.com/Wellner-Head-Extraction/) for more information and usage notes.
--- trunk/my_code/code.py
+++
@@ -1,6 +0,0 @@
-import os
-import gensim
-
-model = gensim.models.word2vec.Word2Vec.load_word2vec_format(os.path.join(os.path.dirname('/home/tarek/Downloads/GoogleNews-vectors-negative300.bin'), 'GoogleNews-vectors-negative300.bin'), binary=True)
-
-model.most_similar('dog')
