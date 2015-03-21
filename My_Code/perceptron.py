3acdac76acf599be905c64186da410536dc510d6
tareksakakini <tarek.sakakini@gmail.com>
2015-03-21 17:37:22 +0100

    update

--- My_Code/perceptron.py
+++ My_Code/perceptron.py
@@ -1,5 +1,57 @@
+import json
 import os
 from collections import defaultdict
+from collections import OrderedDict
+
+def ParseRaw(input_path, output_path, pdtb_dir):
+        os.system(pdtb_dir+"/pdtb-parser/src/parse.rb "+ input_path +" > " + output_path)
+
+def ParseOutput(input_file, output_file, pdtb_dir):
+
+	args=open(output_file,'w')
+
+        os.system(pdtb_dir+"/pdtb-parser/src/parse.rb "+ input_file +" > temp_file")
+
+	parse_output = open('temp_file','r')        
+	Collector = defaultdict(int)
+        Output = defaultdict(int)
+
+        for i in parse_output.readlines():
+                for k in i.split():
+                        if k[0]=='{':
+                                key = k[1:].split('_')
+                                if (len(key)>=3 and (key[2]=="Arg1" or key[2]=="Arg2")):
+                                        Collector[(key[0],key[1],key[2])]={"RawText": ""}
+                        elif k[-1]=='}':
+                                key=k[0:-1].split('_')
+                                if (len(key)>=3 and (key[2]=="Arg1" or key[2]=="Arg2")):
+                                        Output[(key[0],key[1],key[2])] = Collector[(key[0],key[1],key[2])]
+                                        del Collector[(key[0],key[1],key[2])]
+                        else:
+                                for keys in Collector:
+                                        Collector[keys]["RawText"]+=k+" "
+
+
+        jsonDict = defaultdict(int)
+
+        sortedOutput = OrderedDict(sorted(Output.items(), key=lambda t: (t[0],t[1])))
+
+        temp_keys = ("bla","bla")
+        i=-1
+        for keys in sortedOutput:
+		print keys
+                if (keys[0:2]!=temp_keys):
+                        i+=1
+                    	jsonDict[i]=defaultdict(int)
+                        jsonDict[i][keys[2]]=Output[keys]
+                        temp_keys=keys[0:2]
+		else:
+			jsonDict[i][keys[2]]=Output[keys]
+        for keys in jsonDict:
+                args.write(json.dumps(jsonDict[keys]))
+                args.write('\n')
+
+
 
 def PopulateVocabulary():
 	Vocabulary = defaultdict(int)
@@ -18,8 +70,10 @@ def PopulateVocabulary():
 					#print filename
 	return Vocabulary
 
-Vocab = PopulateVocabulary()
-print Vocab['the']
-print Vocab['hence']
-print Vocab['girl']	
-print len(Vocab)
+
+ParseOutput("test1", "args", "../..")
+#Vocab = PopulateVocabulary()
+#print Vocab['the']
+#print Vocab['hence']
+#print Vocab['girl']	
+#print len(Vocab)
